72bbb356a93bcba998b7ac3cf0920033198909d8
Bas Hoonhout <bas@hoonhout.com>
2016-07-18 20:38:55 +0200

    Corrected paths

--- sandmotor/sandmotor-T3/Tdry08.sh
+++ sandmotor/sandmotor-T3/Tdry08.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source ~/.envs/aeolis/bin/activate
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma010.00_Tdry008.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma012.00_Tdry008.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma014.00_Tdry008.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma016.00_Tdry008.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma018.00_Tdry008.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma020.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma010.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma012.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma014.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma016.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma018.00_Tdry008.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma020.00_Tdry008.00_T003.00.txt
--- sandmotor/sandmotor-T3/Tdry10.sh
+++ sandmotor/sandmotor-T3/Tdry10.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 source ~/.envs/aeolis/bin/activate
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma004.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma006.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma008.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma010.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma012.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma014.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma016.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma018.00_Tdry010.00_T003.00.txt
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sigma020.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma004.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma006.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma008.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma010.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma012.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma014.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma016.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma018.00_Tdry010.00_T003.00.txt
+aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma020.00_Tdry010.00_T003.00.txt
