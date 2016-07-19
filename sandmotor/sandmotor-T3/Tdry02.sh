fa7a84d23f44b55c8952bf380838df77ecfcf9c6
Bas Hoonhout <bas@hoonhout.com>
2016-07-19 10:20:57 +0200

    Updated todo lists

--- sandmotor/sandmotor-T3/Tdry02.sh
+++ sandmotor/sandmotor-T3/Tdry02.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source ~/.envs/aeolis/bin/activate
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma010.00_Tdry002.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma012.00_Tdry002.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma014.00_Tdry002.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma016.00_Tdry002.00_T003.00.txt
--- sandmotor/sandmotor-T3/Tdry10.sh
+++ sandmotor/sandmotor-T3/Tdry10.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source ~/.envs/aeolis/bin/activate
-aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma004.00_Tdry010.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma006.00_Tdry010.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma008.00_Tdry010.00_T003.00.txt
 aeolis ~/src/aeolis-models/sandmotor/sandmotor-T3/sandmotor_sigma010.00_Tdry010.00_T003.00.txt
